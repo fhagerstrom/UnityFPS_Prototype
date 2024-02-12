c52f862181cb424b3a675e767a75d4b258703a58
Fabian Hagerström <fabianhagerstrom@hotmail.com>
2024-02-12 11:58:15 +0100

    Update: Level design
    Added interactable door and updated it's prefab. TODO: Open animation for door. Continue working on level.

--- Assets/Scripts/Interactables/Door.cs
+++ Assets/Scripts/Interactables/Door.cs
@@ -1,22 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
-public class AINavigation : MonoBehaviour
+public class Door : Interactable
 {
-    public Transform player;
-    private NavMeshAgent agent;
-
     // Start is called before the first frame update
     void Start()
     {
-        agent = GetComponent<NavMeshAgent>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.destination = player.position;
+        
     }
+
+    protected override void Interact()
+    {
+        Debug.Log("Interacted with and destroyed: " + gameObject.name);
+        Destroy(gameObject);
+    }
+
 }
